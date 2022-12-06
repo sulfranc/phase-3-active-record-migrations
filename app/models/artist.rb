192df64f72f6db946e6751f1f3a364934a203989
sulfranc <sulfranc@gmail.com>
2022-12-05 23:13:32 -0500

    commit tree 21fce1b25797fc584d5570447c9043eb8fd71b5b

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,11 @@
 # add the Artist class here
+
+class Artist < ActiveRecord::Base
+    Artist
+end
+
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[6.1]
+    def change
+      add_column :artists, :favorite_food, :string
+    end
+  end
